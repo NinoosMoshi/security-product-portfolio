d2e3f58599366b0e145e7232175bfa952ed276de
NinoosMoshi <nalkaton@gmail.com>
2021-12-08 09:31:09 -0500

    Spring User class #2

---
+++ backEnd/signin-signup/src/main/java/com/ninos/model/security/User.java
@@ -0,0 +1,32 @@
+package com.ninos.model.security;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "active")
+    private int active;
+
+    private Set<Authorities> authorities = new HashSet<>();
+
+}
