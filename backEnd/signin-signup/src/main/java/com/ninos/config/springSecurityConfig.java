5199e9c82726cebd9b0388f6017174d645e3a0a7
NinoosMoshi <nalkaton@gmail.com>
2021-12-09 09:00:00 -0500

    Spring springSecurityConfig Closes #10

---
+++ backEnd/signin-signup/src/main/java/com/ninos/config/springSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.ninos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class springSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic();
+    }
+
+
+
+
+}
+
